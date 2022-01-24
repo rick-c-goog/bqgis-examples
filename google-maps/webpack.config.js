30e5006aece2185cebb5e25aacf3b75ca1a6cba8
Rick(Rugui) Chen <rickruguichen@google.com>
2022-01-24 12:04:34 -0500

    yarn file

--- google-maps/webpack.config.js
+++ google-maps/webpack.config.js
@@ -10,6 +10,11 @@ const CONFIG = {
     app: './app.js'
   },
 
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+    "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+  },
   plugins: [
     // Read google maps token from environment variable
     new webpack.EnvironmentPlugin(['GoogleMapsAPIKey', 'GoogleMapsMapId'])
